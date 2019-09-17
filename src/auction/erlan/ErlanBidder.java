fc40379182ca4c6619e168929f11def9905bb1b3
Erlan Moldaliev <erlan.moldaliev@gmail.com>
2019-09-17 22:44:53 +0200

    Update ErlanBidder.java

--- src/auction/erlan/ErlanBidder.java
+++ src/auction/erlan/ErlanBidder.java
@@ -5,7 +5,12 @@ import auction.Bidder;
 public class ErlanBidder implements Bidder {
 
     private int quantity = 0;
+    private int myQuantity = 0;
+
     private int cash = 0;
+
+    private int totalRounds = 0;
+    private int currentRound = 0;
 
     public ErlanBidder() {
         //nothing happens;
@@ -15,12 +20,34 @@ public class ErlanBidder implements Bidder {
     public void init(int quantity, int cash) {
         this.quantity = quantity;
         this.cash = cash;
+
+        if(quantity%2==0)
+        {
+            this.totalRounds = quantity/2;
+        } else {
+            this.totalRounds = quantity/2 + 1;
+        }
     }
 
     @Override
     public int placeBid() {
+        if(0==cash || currentRound==totalRounds)
+        {
+            return 0;
+        }
 
-        return 0;
+        if(0==currentRound)
+        {
+            return 0;
+        }
+
+        int bid = cash/(totalRounds - currentRound) + 1;
+        if(bid<=cash)
+        {
+            return bid;
+        } else {
+            return cash;
+        }
     }
 
     @Override

be0ea1e4f2a374dc508efff0e2fc451548465d63
erlansan <erlan.moldaliev@gmail.com>
2019-09-17 21:58:48 +0200

    Created packages

--- src/auction/erlan/ErlanBidder.java
+++ src/auction/erlan/ErlanBidder.java
@@ -1,11 +1,25 @@
+package auction.erlan;
+
+import auction.Bidder;
+
 public class ErlanBidder implements Bidder {
+
+    private int quantity = 0;
+    private int cash = 0;
+
+    public ErlanBidder() {
+        //nothing happens;
+    }
+
     @Override
     public void init(int quantity, int cash) {
-
+        this.quantity = quantity;
+        this.cash = cash;
     }
 
     @Override
     public int placeBid() {
+
         return 0;
     }
 
